9965a7cb1ff3848c7c4b755a3a5db51fce27124b
Fedor Grab <fvgrab@gmail.com>
2022-01-06 01:36:58 -0500

    Get rid of opencv

--- garbage_server/constants.py
+++ garbage_server/constants.py
@@ -1,5 +1,6 @@
 MODEL_PATH = "./model.h5"
 DATA_SAVING_PATH = "./data"
 IMAGE_SIZE = (224, 224)
+IMAGE_SHAPE = IMAGE_SIZE + (3,)
 CLASSES = ['cardboard', 'compost', 'glass', 'metal', 'other', 'paper', 'plastic',
            'service_gesture', 'trash']
--- garbage_server/utils.py
+++ garbage_server/utils.py
@@ -1,5 +1,6 @@
 import numpy as np
-import cv2
+import tensorflow as tf
+from tensorflow.image import decode_jpeg, resize
 from PIL import Image
 from io import BytesIO
 import constants
@@ -11,7 +12,7 @@ def load_image_into_numpy_array(data) -> np.array:
 
 def load_images_as_tensor(data) -> np.array:
     images = [
-        cv2.resize(np.array(Image.open(BytesIO(img_bytes))), dsize=constants.IMAGE_SIZE)
+        resize(decode_jpeg(img_bytes), constants.IMAGE_SIZE)
         for img_bytes in data
     ]
-    return np.stack(images)
+    return tf.stack(images)
